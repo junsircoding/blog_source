62caaad5e2e4d494abc7808fa36dcf5256871f91
junsircoding <junsircoding@outlook.com>
2024-06-17 09:40:43 +0800

    update 20230722.

--- restart.sh
+++ restart.sh
@@ -19,6 +19,7 @@ make html
 
 cp ${WORKDIR}/source/BingSiteAuth.xml  ${WORKDIR}/build/html
 cp ${WORKDIR}/source/google79cba44476b50244.html  ${WORKDIR}/build/html
+cp ${WORKDIR}/source/ads.txt  ${WORKDIR}/build/html
 cp ${WORKDIR}/source/sitemap.xml ${WORKDIR}/build/html
 cp -r ${WORKDIR}/source/storage  ${WORKDIR}/build/html
 
