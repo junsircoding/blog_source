e271634428f96b4cb6442f9e30f3a8f169509ea7
wangjun <wangjun@youjivest.com>
2025-03-12 15:54:42 +0800

    修改svg图标配色.

--- restart.sh
+++ restart.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-WORKDIR=/home/junsircoding/Documents/blog_source/docs
-# 查询并终止 main.py 的进程
-PID=$(ps aux | grep "python -m http.server" | grep -v grep | awk '{print $2}')
-echo $PID
-if [ -n "$PID" ]; then
-    echo "停止进程: $PID"
-    kill -9 $PID
-else
-    echo "服务未运行"
-fi
+WORKDIR=/home/$(whoami)/Documents/blog_source/docs
 
-source /home/junsircoding/miniforge3/bin/activate blog
+source /home/$(whoami)/miniforge3/bin/activate blog
 rm -rf ${WORKDIR}/build
 
 cd ${WORKDIR}
@@ -22,9 +13,3 @@ cp ${WORKDIR}/source/google79cba44476b50244.html  ${WORKDIR}/build/html
 cp ${WORKDIR}/source/ads.txt  ${WORKDIR}/build/html
 cp ${WORKDIR}/source/sitemap.xml ${WORKDIR}/build/html
 cp -r ${WORKDIR}/source/storage  ${WORKDIR}/build/html
-
-# cd ${WORKDIR}/build/html
-# nohup python -m http.server > /dev/null 2>&1 &
-
-# PID=$(ps aux | grep "python -m http.server" | grep -v grep | awk '{print $2}')
-# echo ${PID}
