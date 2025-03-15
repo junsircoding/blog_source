b194cdb8214f890d89ed2ba73bdc8755caa2c492
junsircoding <junsircoding@outlook.com>
2025-03-15 13:24:01 +0800

    增加robots.txt

--- restart.sh
+++ restart.sh
@@ -10,5 +10,6 @@ make html
 
 cp ${WORKDIR}/source/BingSiteAuth.xml  ${WORKDIR}/build/html
 cp ${WORKDIR}/source/sitemap.xml ${WORKDIR}/build/html
+cp ${WORKDIR}/source/robots.txt ${WORKDIR}/build/html
 cp -r ${WORKDIR}/source/storage  ${WORKDIR}/build/html
 cp -r ${WORKDIR}/source/jieba_tmp  ${WORKDIR}/build/html
